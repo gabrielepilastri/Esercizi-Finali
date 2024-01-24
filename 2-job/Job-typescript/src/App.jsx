86c2f0dbb27568b44b6dad76715862017d592940
gabrielepilastri <gabrielepilastri@gmail.com>
2024-01-24 14:54:00 +0100

    commit

--- 2-job/Job-typescript/src/App.jsx
+++ 2-job/Job-typescript/src/App.jsx
@@ -1,20 +1,20 @@
 
-import './App.css'
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home'
+import MyNavbar from './components/MyNavbar';
 
 function App() {
 
   return (
     <>
-      
+      <MyNavbar/>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home/>} />
             
           </Routes>
         </BrowserRouter>
-      
     </>
   )
 }
